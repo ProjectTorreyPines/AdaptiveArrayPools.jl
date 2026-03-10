ba0e6656493c3662f0fae2d6889b517bde38f37c
Min-Gu Yoo <mgyoo86@gmail.com>
2026-03-10 16:40:55 -0700

    feat: add "Pool Safety" section to documentation features list

--- docs/make.jl
+++ docs/make.jl
@@ -92,6 +92,7 @@ makedocs(
             "Multi-threading" => "features/multi-threading.md",
         ],
         "Features" => [
+            "Pool Safety" => "features/safety.md",
             "`@maybe_with_pool`" => "features/maybe-with-pool.md",
             "Bit Arrays" => "features/bit-arrays.md",
             "CUDA Support" => "features/cuda-support.md",
